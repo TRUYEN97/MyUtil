6b2d95a49852f24fe4f035301ca01e19b2df474f
SQT <samtruyen60@mail.com>
2024-01-23 08:32:13 +0700

    update

--- src/main/java/com/tec02/common/APIController.java
+++ src/main/java/com/tec02/common/APIController.java
@@ -2,7 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.tec02.API;
+package com.tec02.common;
+
+import com.tec02.API.RestAPI;
 
 
 
